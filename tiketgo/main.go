ef65677444bb469c6ffc373131b6bf871dad6e02
Restu Setiawan <56220861+ReztuZt@users.noreply.github.com>
2024-01-24 11:39:31 +0700

    Update main.go

--- tiketgo/main.go
+++ tiketgo/main.go
@@ -1,27 +1,38 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
 )
- 
+
 func main() {
-	// Mengatur penanganan file statis
+	// Mengatur penanganan file statis(biar folder static terbaca)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
 	// Menentukan penanganan untuk halaman utama
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// Parsing templat HTML
+		// Parsing templat HTML(nantine akan dieksekusi ning ngisor)
 		tmpl, err := template.ParseFiles("templates/index.html")
 		if err != nil {
 			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			fmt.Println("Error parsing template:", err)
 			return
 		}
 
-		// Menyajikan templat HTML
-		tmpl.Execute(w, nil)
+		// Menyajikan templat HTML(mengeksekusi index.html)
+		err = tmpl.Execute(w, nil)
+		if err != nil {
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			fmt.Println("Error executing template:", err)
+			return
+		}
 	})
 
-	// Menjalankan server di port 8080
-	http.ListenAndServe(":8082", nil)
+	// Menjalankan server di port 8082(wis ngerti lah yaa)
+	fmt.Println("Server berhasil dijalankan di http://localhost:8082")
+	err := http.ListenAndServe(":8082", nil)
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
 }
